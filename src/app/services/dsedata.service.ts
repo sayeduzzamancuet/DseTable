850ac08c422446d4d2d19da6a4c4ccb6037531fe
SAYED UZ ZAMAN <34524280+sayeduzzamancuet@users.noreply.github.com>
2022-02-25 23:29:49 +0600

    api change

--- src/app/services/dsedata.service.ts
+++ src/app/services/dsedata.service.ts
@@ -12,8 +12,8 @@ export class DsedataService {
   data:any;
     apiUrl:string='http://www.dsebd.org:80/DSEApps/filter.php?key=mkistat_total_volume';
    //apiUrl:string='https://demo123.free.beeceptor.com/';
-   apiUrl2:string='https://localhost:5001';
-   //apiUrl2:string='https://stanalytics2022.herokuapp.com';
+   //apiUrl2:string='https://localhost:5001';
+   apiUrl2:string='https://stanalytics2022.herokuapp.com';
   constructor(private httpClient:HttpClient) { }
   getStockData():Observable<StockData>{
      return this.httpClient.get<StockData>(this.apiUrl);
